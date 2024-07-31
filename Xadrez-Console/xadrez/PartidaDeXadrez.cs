aa40ab528c37fd35a8d0c644e2f7da4afbf67296
Victor Coronet <victorcoronetaraujo@hotmail.com>
2024-07-31 11:20:00 -0300

    Fazendo os primeiros testes de movimentacao

---
+++ Xadrez-Console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set; }
+        private int turno;
+        private Cor jogadorAtual;
+        public bool terminada {  get; private set; }
+
+        public PartidaDeXadrez() 
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+            colocarPecas();
+            terminada = false;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino) 
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p, destino);
+
+        }
+        private void colocarPecas() 
+        {
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+        }
+    }
+}
